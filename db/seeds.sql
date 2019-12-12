192617140cc1ebc051c22cf8cc3982bda2103a4b
Ajay Singh <ajayh2@gmail.com>
2019-12-11 21:48:44 -0500

    suprise

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('jalopeno');
+INSERT INTO burgers (burger_name) VALUES ('peanut-butter');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken', true);
+
